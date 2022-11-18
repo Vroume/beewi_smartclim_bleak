5e007ebf44683793e9a4ea66c76ea9e4d76e155f
Vroume <vroumezin@gmail.com>
2022-11-18 23:39:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="beewi_smartclim",
+    name="beewi_smartclim_bleak",
     version="0.0.1",
     author="Vroume",
     author_email="none",
