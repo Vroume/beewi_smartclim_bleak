6f44563fad4d8793c7eddc3899e9997878a23d22
Vroume <vroumezin@gmail.com>
2022-11-18 22:18:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from beewi_smartclim import BeewiSmartClimPoller
 
-b = BeewiSmartClimPoller("d0:5f:b8:51:9b:36")
+b = BeewiSmartClimPoller("78:A5:04:5A:9E:1D")
 b.update_sensor()
 
 temperature = b.get_temperature()
